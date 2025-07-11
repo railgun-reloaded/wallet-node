a33861be82d3dd6db9f4451a51c353837e74a859
zy0n.bear <zy0n@users.noreply.github.com>
2025-07-11 01:53:04 -0400

    feat: Add AES decryption method to WalletNode and update cryptography imports

--- src/wallet-node/wallet-node.ts
+++ src/wallet-node/wallet-node.ts
@@ -1,4 +1,4 @@
-import { poseidon } from '@railgun-reloaded/cryptography'
+import { AES, poseidon } from '@railgun-reloaded/cryptography'
 
 import { getPublicSpendingKey, getPublicViewingKey } from '../keys'
 import { childKeyDerivationHardened, getMasterKeyFromSeed, getPathSegments } from '../seed/bip32'
@@ -24,11 +24,6 @@ type WalletNodes = { spending: WalletNode; viewing: WalletNode }
  * ### Usage:
  * This class is designed for use in cryptographic wallet implementations, enabling secure
  * key management and hierarchical deterministic wallet functionality.
- *
- * ### Dependencies:
- * - `poseidonFunc`: A cryptographic hash function used for key computations.
- * - `uint8ArrayToBigInt`: Utility for converting Uint8Array to bigint.
- * - External functions for mnemonic seed generation, key derivation, and public key computation.
  *
  * ### Notes:
  * - Ensure proper handling of Uint8Array and bigint conversions in cryptographic operations.
@@ -156,6 +151,33 @@ class WalletNode {
     // const uint8Array = [uint8ArrayToBigInt(privateKey)]
     // TODO: properly do this, as its being 'set to' uint8 array inside here, and now revisded as bigint
     return poseidon([privateKey])
+  }
+
+  /**
+   * Decrypts a randomly encrypted bundle using AES-GCM.
+   * @param encryptedBundle - An array containing three Uint8Array elements:
+   *   - The first element represents the IV and authentication tag.
+   *   - The second element contains the encrypted data.
+   *   - The third element is unused in this function.
+   * @param sharedKey - A Uint8Array representing the shared key used for decryption.
+   * @returns The decrypted data as a Uint8Array.
+   * @throws Will throw an error if decryption fails or the input data is invalid.
+   */
+  decryptRandom (
+    encryptedBundle: [Uint8Array, Uint8Array, Uint8Array],
+    sharedKey: Uint8Array
+  ): Uint8Array {
+    const hexlified0 = encryptedBundle[0]
+    const hexlified1 = encryptedBundle[1]
+    const decrypted = AES.decryptGCM(
+      {
+        iv: hexlified0.slice(0, 16),
+        tag: hexlified0.slice(16, 32),
+        data: [hexlified1.slice(0, 16)],
+      },
+      sharedKey
+    )[0]!
+    return decrypted
   }
 }
 
